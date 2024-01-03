a53da4dfdd3b276794b9d8070baa2d642bfb56f3
Nekunj Khanna <kuvam654@gmail.com>
2024-01-03 13:07:48 +0530

    file updated

---
+++ Comments_helloworld.js
@@ -0,0 +1,16 @@
+//comments
+//single line comments
+
+//multiple line comments
+/* 
+this is a 
+multiple line
+comment
+*/
+
+// Semi Colon - Using the semi colon we tell java that we have ended a line of code.
+// ASI - Automatic Semi Colon Insertion - Browsers have this feature which lets us continue without putting a semi colon.
+
+//Hello World Program
+console.log("hello world");
+
